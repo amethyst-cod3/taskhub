d8d94444df6f77798b0ad69b8146002eaa9a624e
amethyst-cod3 <raulsamt02@gmail.com>
2024-06-08 01:48:51 +0200

    Loading widget created

---
+++ lib/components/loading.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:taskhub/styles/colors.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: CustomColor.lightwhite,
+      child: const Center(
+        child: SpinKitFoldingCube(
+          color: CustomColor.darkblue,
+          size: 50,
+        ),
+      ),
+    );
+  }
+}
